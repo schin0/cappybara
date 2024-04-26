5ed54dfa1a3b1151f8c08a718b59a15d52030ee9
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-26 14:58:20 -0300

    Finalizar ajustes no menu

--- app/src/main/java/br/com/invocoders/cappybara/screens/MenuScreen.kt
+++ app/src/main/java/br/com/invocoders/cappybara/screens/MenuScreen.kt
@@ -1,19 +1,29 @@
 package br.com.invocoders.cappybara.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
 import br.com.invocoders.cappybara.R
 
 @Composable
@@ -23,33 +33,46 @@ fun MenuScreen(navController: NavController) {
             .fillMaxSize(),
         contentAlignment = Alignment.BottomCenter
     ) {
-
         Row(
             modifier = Modifier
-                .fillMaxWidth(),
+                .fillMaxWidth()
+                .background(Color.White)
+                .padding(16.dp),
             horizontalArrangement = Arrangement.SpaceEvenly
         ) {
-            Button(onClick = {
-                navController.navigate("menu")
-            }) {
+            Button(
+                onClick = {
+                    navController.navigate("menu")
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.Black),
+                shape = RoundedCornerShape(10.dp)
+            ) {
                 Image(
-                    painter = painterResource(id = R.drawable.baseline_home_24),
+                    painter = painterResource(id = R.drawable.baseline_home_24_white),
                     contentDescription = "Ícone home"
                 )
             }
 
-            Button(onClick = {
-                navController.navigate("pesquisa")
-            }) {
+            Button(
+                onClick = {
+                    navController.navigate("pesquisa")
+                },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, Color.Black)
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.baseline_search_24),
                     contentDescription = "Ícone home"
                 )
             }
 
-            Button(onClick = {
-                navController.navigate("menu")
-            }) {
+            Button(
+                onClick = { },
+                colors = ButtonDefaults.buttonColors(containerColor = Color.White),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(1.dp, Color.Black)
+            ) {
                 Image(
                     painter = painterResource(id = R.drawable.baseline_calendar_month_24),
                     contentDescription = "Ícone home"
@@ -57,4 +80,11 @@ fun MenuScreen(navController: NavController) {
             }
         }
     }
+}
+
+@Preview
+@Composable
+private fun PreviewMenuScreen() {
+    var nav = rememberNavController()
+    MenuScreen(nav)
 }
