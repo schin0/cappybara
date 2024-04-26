1431f5a014228680406ada8a43af53e0b49b91a2
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-26 20:43:02 -0300

    Resolver warnings

--- app/src/main/java/br/com/invocoders/cappybara/MainActivity.kt
+++ app/src/main/java/br/com/invocoders/cappybara/MainActivity.kt
@@ -5,16 +5,11 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
