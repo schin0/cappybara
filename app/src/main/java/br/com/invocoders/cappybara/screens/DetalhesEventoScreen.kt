3772466ec979133e9ce331f5957ed79fff04c1cb
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-28 15:27:14 -0300

    Remover warnings

--- app/src/main/java/br/com/invocoders/cappybara/screens/DetalhesEventoScreen.kt
+++ app/src/main/java/br/com/invocoders/cappybara/screens/DetalhesEventoScreen.kt
@@ -1,7 +1,6 @@
 package br.com.invocoders.cappybara.screens
 
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
