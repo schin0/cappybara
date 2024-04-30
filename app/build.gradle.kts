9933f7a47549002bd0682bce68d8ab85e749d8d7
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-29 21:39:31 -0300

    Remover dependências

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -60,7 +60,7 @@ dependencies {
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
     implementation(libs.androidx.navigation)
-    implementation(libs.play.services.maps)
+//    implementation(libs.play.services.maps)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
