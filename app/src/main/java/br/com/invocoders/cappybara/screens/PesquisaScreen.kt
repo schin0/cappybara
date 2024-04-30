463130b21634f5221c9e2bb7c046ba060590105a
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-29 21:55:00 -0300

    Adicionar campos de pesquisa

--- app/src/main/java/br/com/invocoders/cappybara/screens/PesquisaScreen.kt
+++ app/src/main/java/br/com/invocoders/cappybara/screens/PesquisaScreen.kt
@@ -1,9 +1,75 @@
 package br.com.invocoders.cappybara.screens
 
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import br.com.invocoders.cappybara.R
+import br.com.invocoders.cappybara.components.BotaoComponente
+import br.com.invocoders.cappybara.model.MenuItem
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun PesquisaScreen(navController: NavController) {
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(start = 32.dp, top = 16.dp, end = 32.dp, bottom = 64.dp)
+    ) {
+        Spacer(modifier = Modifier.height(32.dp))
+
+        Row(
+            modifier = Modifier.fillMaxWidth(),
+            verticalAlignment = Alignment.CenterVertically,
+            horizontalArrangement = Arrangement.SpaceBetween
+        ) {
+            OutlinedTextField(
+                value = "",
+                onValueChange = { },
+                enabled = false,
+                placeholder = {
+                    Text(text = "Pesquise pelo nome do evento")
+                },
+                shape = RoundedCornerShape(20.dp),
+                modifier = Modifier
+                    .size(230.dp, 40.dp)
+                    .padding(0.dp),
+                colors = TextFieldDefaults.outlinedTextFieldColors(
+                    Color.Black,
+                    disabledTextColor = Color.Black,
+                    disabledBorderColor = Color.Black,
+                    disabledLabelColor = Color.Black,
+                )
+            )
+
+            BotaoComponente(
+                item = MenuItem("pesquisa", R.drawable.baseline_search_24, R.drawable.baseline_search_24_white, "pesquisa"),
+                selecionado = true,
+                navController = navController
+            )
+        }
+    }
+
     MenuScreen(navController, "pesquisa")
 }
