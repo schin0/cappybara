da805245c2b06efdba9f2f1769e49d08ee4ffd50
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-04-29 21:55:58 -0300

    Resolver warnings

--- app/src/main/java/br/com/invocoders/cappybara/screens/PesquisaScreen.kt
+++ app/src/main/java/br/com/invocoders/cappybara/screens/PesquisaScreen.kt
@@ -14,7 +14,6 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.OutlinedTextFieldDefaults
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
