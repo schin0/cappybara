c9bda86c6b0c6525204ed3ec6aba8f692b839f63
Gabriel Schincariol <Gabriel.Schincariol@fagrontech.com.br>
2024-07-25 00:01:29 -0300

    Finalizar header

--- app/src/main/java/br/com/invocoders/cappybara/components/BotaoMenuComponent.kt
+++ app/src/main/java/br/com/invocoders/cappybara/components/BotaoMenuComponent.kt
@@ -1,16 +1,13 @@
 package br.com.invocoders.cappybara.components
 
-import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
-import androidx.compose.material3.ButtonColors
 import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,7 +45,10 @@ fun BotaoMenuComponent(item: MenuItem, selecionado: Boolean, navController: NavC
             containerColor = Color.Transparent,
             contentColor = Color.Black
         ),
-        shape = RoundedCornerShape(10.dp)
+        shape = RoundedCornerShape(10.dp),
+        modifier = Modifier
+            .fillMaxWidth()
+            .height(80.dp)
     ) {
         Column(
             modifier = Modifier.padding(0.dp),
@@ -71,7 +71,5 @@ fun BotaoMenuComponent(item: MenuItem, selecionado: Boolean, navController: NavC
                 )
             )
         }
-
-
     }
 }
