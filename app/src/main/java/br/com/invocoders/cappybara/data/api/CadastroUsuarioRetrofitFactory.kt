e93cc17e2e0851ed2f52854a77a3fda687025192
Gabriel Schincariol <gschin11@gmail.com>
2025-06-15 01:13:59 -0300

    refactor url api

--- app/src/main/java/br/com/invocoders/cappybara/data/api/CadastroUsuarioRetrofitFactory.kt
+++ app/src/main/java/br/com/invocoders/cappybara/data/api/CadastroUsuarioRetrofitFactory.kt
@@ -5,7 +5,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class CadastroUsuarioRetrofitFactory {
-    private val URL = "httphttp://cappybara-evento-dyagdvfdfbd3hvg3.brazilsouth-01.azurewebsites.net/usuario/"
+    private val URL = "http://cappybara-evento-dyagdvfdfbd3hvg3.brazilsouth-01.azurewebsites.net/usuario/"
     val retrofitFactory = Retrofit.Builder()
         .baseUrl(URL)
         .addConverterFactory(GsonConverterFactory.create())
