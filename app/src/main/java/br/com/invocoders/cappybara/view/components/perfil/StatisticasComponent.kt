73ce4a051d862aaca0edd9fa1b044c03dc375934
Gabriel Schincariol <gschin11@gmail.com>
2025-08-03 15:50:40 -0300

    refactor usings

--- app/src/main/java/br/com/invocoders/cappybara/view/components/perfil/StatisticasComponent.kt
+++ app/src/main/java/br/com/invocoders/cappybara/view/components/perfil/StatisticasComponent.kt
@@ -1,7 +1,14 @@
 package br.com.invocoders.cappybara.view.components.perfil
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -10,7 +17,6 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontFamily
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
--- app/src/main/java/br/com/invocoders/cappybara/view/screens/quebragelo/QuebraGeloScreen.kt
+++ app/src/main/java/br/com/invocoders/cappybara/view/screens/quebragelo/QuebraGeloScreen.kt
@@ -1,20 +1,36 @@
 package br.com.invocoders.cappybara.view.screens.quebragelo
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.shape.RoundedCornerShape
-import androidx.compose.foundation.verticalScroll
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.pager.HorizontalPager
-import androidx.compose.foundation.pager.rememberPagerState
 import androidx.compose.foundation.pager.PagerDefaults
+import androidx.compose.foundation.pager.rememberPagerState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.rounded.ArrowBack
-import androidx.compose.material3.*
-import androidx.compose.runtime.*
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardColors
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.shadow
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.colorResource
@@ -27,9 +43,8 @@ import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import br.com.invocoders.cappybara.R
 import br.com.invocoders.cappybara.model.IdeiaQuebraGelo
-//import br.com.invocoders.cappybara.view.components.shared.BotaoVoltarComponent
-import br.com.invocoders.cappybara.view.components.quebragelo.BotaoGerarNovaSugestaoComponent
 import br.com.invocoders.cappybara.view.components.quebragelo.BotaoEscreverPropriaComponent
+import br.com.invocoders.cappybara.view.components.quebragelo.BotaoGerarNovaSugestaoComponent
 
 @Composable
 fun QuebraGeloScreen(navController: NavController) {
